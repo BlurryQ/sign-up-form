139afdc0392c845b12ab77512a87d2ea1bb34321
BlurryQ <skullsmash@hotmail.co.uk>
2023-12-21 20:56:12 +0000

    Increased line spacing nad added JS

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("This will grant you access to Dobermann pics :D")
+
+const theme = document.getElementById("theme");
+theme.addEventListener("click", () => {
+    console.log("woof");
+    })
+
+const password = document.getElementById("password");
+password.addEventListener("change", () => {
+    string = password.value
+    console.log(string);
+    })
+
+const confirmPassword = document.getElementById("confirm-password");
+confirmPassword.addEventListener("change", () => {
+    string = confirmPassword.value
+    console.log(string);
+    })
+
+const createAccount = document.getElementById("submit")
+createAccount.addEventListener("click", () => {
+    console.log("HERE")
+    if(password === confirmPassword) {
+        console.log("match")
+    } else {
+        console.log("invalid")
+    }
+})
