ed0a5ee112cc8de2b3002bb2599a6782ecf38dc0
BlurryQ <skullsmash@hotmail.co.uk>
2023-12-22 15:10:23 +0000

    Added theme toggle and images

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ console.log("This will grant you access to Dobermann pics :D")
 
 const theme = document.getElementById("theme");
 theme.addEventListener("click", () => {
-    console.log("woof");
+    console.log("woof")
+toggleTheme();
     })
 
 const password = document.getElementById("password");
@@ -26,3 +27,9 @@ createAccount.addEventListener("click", () => {
         console.log("invalid")
     }
 })
+
+function toggleTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === "dark" ? "light" : "dark";
+    root.className = newTheme;
+}
